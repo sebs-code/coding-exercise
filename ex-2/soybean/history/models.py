a31c516ae836854291c37a10a429dff4383d3211
mk <mkirkwood@triller.co>
2020-07-24 15:44:40 +0100

    SoybeanCondition model and ListView with template.

--- ex-2/soybean/history/models.py
+++ ex-2/soybean/history/models.py
@@ -15,3 +15,15 @@ PLEASE NOTE: For 'Condition', choices can be from 'Very Poor', 'Poor', 'Fair',
  'Good', 'Excellent'
 """
 
+
+class SoybeanCondition(models.Model):
+    class Condition(models.TextChoices):
+        VERY_POOR = 'Very Poor', 'Very Poor'
+        POOR = 'Poor', 'Poor'
+        FAIR = 'Fair', 'Fair'
+
+    week_ending = models.DateField()
+    condition = models.CharField(choices=Condition.choices, max_length=30)
+    average_temperature = models.DecimalField(max_digits=4, decimal_places=1)
+    grade = models.IntegerField()
+    inspector = models.CharField(max_length=50)
--- ex-2/soybean/history/views.py
+++ ex-2/soybean/history/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import SoybeanCondition
+
+
+class SoybeanConditionListView(ListView):
+    queryset = SoybeanCondition.objects.all()
